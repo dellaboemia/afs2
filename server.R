faddaf79f1ae959fb3e57cdee12c6539767f1880
John James <dellaboemia@gmail.com>
2016-05-20 11:09:40 -0400

    Working with last fuel type selected

--- server.R
+++ server.R
@@ -4,56 +4,121 @@
 #John James
 #May 13, 2016
 
+# Libraries
+library(RCurl)
+library(curl)
+library(datasets)
+library(dplyr)
+library(jsonlite)
+library(leaflet)
+library(rjson)
+library(shiny)
+library(zipcode)
+
+#Global Variables
+## Base URL
+afsBaseURL <- "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=zhiWRDbKkuL7G0Iwm2IifkfxfBeqcJ46GaHQnv5E"
+
+##User's location based upon their IP address
+ipInfo  <- getURL("ipinfo.io/loc")
+loc     <- gsub("[\n]","",ipInfo)
+
+##User's Current longitude and latitude 
+homeLat  <- strsplit(loc, ",")[[1]][1]
+homeLong <- strsplit(loc, ",")[[1]][2]
+
+##User's home URL for nearest stations
+homeURL <- paste(afsBaseURL,"&latitude=",homeLat,"&longitude=",homeLong, sep = "")
+
+## Fuel Types
+fuelTypes <- c("BD", "CNG", "E85", "ELEC", "HY", "LNG", "LPG")
+
+#Main server processing
 shinyServer(function(input, output, session) {
   
-  # Render list of states
-  output$stateUi <- renderUI({
-    selectInput("state", label = ("State:"), c(Choose='', state.name), selectize = FALSE)
-  })
-  
-  # Render list of cities for state selected
-  output$cityUi <- renderUI({
-    selectInput("city", label = "City",choices = c(Choose='',unique(subset(zipcode, State.Name == input$state)$City)), 
-                selectize = FALSE)
-  })
-  
-  # Render zip code field
-  output$zipUi <- renderUI({
-    textInput(inputId = "zip", label = "Zipcode:")
-  })
-  
-  
-  # Get longitude and latitude for state and city selected
-  getCenter <- eventReactive(input$refresh, {
-    if (nrow(subset(zipcode, Zip == input$zip)) > 0) {
-      subset(zipcode, Zip == input$zip)
+  formatSearchURL <- reactive({
+    
+    # Form fuel type query string option
+    if (input$BD == TRUE) { fuelTypeBD <- "&fuel_type=BD" } else {fuelTypeBD <- ""}
+    if (input$CNG == TRUE) { fuelTypeCNG <- "&fuel_type=CNG" } else {fuelTypeCNG <- ""}
+    if (input$E85 == TRUE) { fuelTypeE85 <- "&fuel_type=E85" } else {fuelTypeE85 <- ""}
+    if (input$ELEC == TRUE) { fuelTypeELEC <- "&fuel_type=ELEC" } else {fuelTypeELEC <- ""}
+    if (input$HY == TRUE) { fuelTypeHY <- "&fuel_type=HY" } else {fuelTypeHY <- ""}
+    if (input$LNG == TRUE) { fuelTypeLNG <- "&fuel_type=LNG" } else {fuelTypeLNG <- ""}
+    if (input$LPG == TRUE) { fuelTypeLPG <- "&fuel_type=LPG" } else {fuelTypeLPG <- ""}
+    
+    fuelTypeOption <- paste(fuelTypeBD, fuelTypeCNG, fuelTypeE85, fuelTypeELEC,  
+                              fuelTypeHY, fuelTypeLNG, fuelTypeLPG, sep = "")
+    
+    
+    # Format search string
+    if (input$refresh[1] == 0 | input$currentLocation == TRUE) {
+      #First time or use current location is selected
+      paste(homeURL, fuelTypeOption, sep = "")
     } else {
-      subset(zipcode, City==input$city & State.Name==input$state)[1,]
-    }}, ignoreNULL = FALSE)
-  
-  output$m <- renderLeaflet({
-    cols <- rainbow(length(levels(afs$Fuel.Type.Code)), alpha = NULL)
-    afs$colors <- cols[unclass(afs$Fuel.Type.Code)]
-    if (input$refresh[1] == 0) {
-      leaflet()  %>%
-        addTiles() %>%
-        setView(lng = homeLong, lat = homeLat, zoom = 15) %>%
-        addCircleMarkers(data = afs, lat = ~ Latitude, lng = ~ Longitude, popup = ~Station.Name, color = ~colors) %>%
-        addLegend(position = "bottomright", labels = fuel_types, colors = cols)       
-    } else {     
-      leaflet()  %>%
-        addTiles() %>%
-        setView(lng = getCenter()$Longitude, lat = getCenter()$Latitude, zoom = 15) %>%
-        addCircleMarkers(data = afs, lat = ~ Latitude, lng = ~ Longitude, popup = ~Station.Name, color = ~colors) %>%
-        addLegend(position = "bottomright", labels = fuel_types, colors = cols)
-  }})
-  
-  output$table <- DT::renderDataTable({
-    if (nrow(subset(afs, ZIP == input$zip)) !=0) {
-      x <- subset(afs, ZIP == input$zip)
-    } else {
-      x <- subset(afs, City == input$city & State.Name == input$state)
+      #Not first time, format location parameter
+      location <- gsub(" ","+",input$location)
+      paste(afsBaseURL, "&location=", location, fuelTypeOption, sep = "")
     }
-    DT::datatable(x[vars],options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
+    
   })
+
+  
+  formatStations <- function(afs){
+    name            <- unlist(sapply(afs$fuel_stations, function(x) x$station_name))
+    distance        <- unlist(sapply(afs$fuel_stations, function(x) x$distance))
+    fuelType        <- unlist(sapply(afs$fuel_stations, function(x) x$fuel_type_code))
+    address         <- unlist(sapply(afs$fuel_stations, function(x) x$street_address))
+    city            <- unlist(sapply(afs$fuel_stations, function(x) x$city))
+    state           <- unlist(sapply(afs$fuel_stations, function(x) x$state))
+    zip             <- unlist(sapply(afs$fuel_stations, function(x) x$zip))
+    phone           <- unlist(sapply(afs$fuel_stations, function(x) x$station_phone))
+    hours           <- unlist(sapply(afs$fuel_stations, function(x) x$access_days_time))
+    latitude        <- unlist(sapply(afs$fuel_stations, function(x) x$latitude))
+    longitude       <- unlist(sapply(afs$fuel_stations, function(x) x$longitude))
+    stations        <- data.frame(name, distance, fuelType, address, city, state, zip, phone, hours, latitude, longitude)
+  }
+  # Get alternative fuel stations based upon user's input or location
+  output$m <- renderLeaflet({
+    
+    # Retrieve searchURL
+    searchURL <- formatSearchURL()
+    print(searchURL)
+
+    # Get alternative fuel stations 
+    afsJSON <- getURL(searchURL)
+    afs     <- fromJSON(afsJSON)
+    
+    # Format station information
+    stations <- formatStations(afs)
+    print(stations)
+
+    #Format colors for legend
+    cols <- rainbow(length(levels(stations$fuelType)), alpha = NULL)
+    stations$colors <- cols[unclass(stations$fuelType)]
+ 
+    #Format map
+    if (is.null(stations$latitude)) {
+      leaflet(stations)  %>%
+        addTiles() %>%
+        setView(lng = afs$longitude, lat = afs$latitude, zoom = 15)
+    } else {
+      leaflet(stations)  %>%
+        addTiles() %>%
+        setView(lng = afs$longitude, lat = afs$latitude, zoom = 15) %>%
+        fitBounds(~min(longitude), ~min(latitude), ~max(longitude), ~max(latitude)) %>%
+        addCircleMarkers(data = stations, lat =  ~latitude, lng = ~longitude, popup = ~name, color = ~colors) %>%
+        addLegend(position = "bottomright", labels = unique(stations$fuelType), colors = cols)
+    }
+
+  })
+
+  # output$table <- DT::renderDataTable({
+  #   if (nrow(subset(afs, ZIP == input$zip)) !=0) {
+  #     x <- subset(afs, ZIP == input$zip)
+  #   } else {
+  #     x <- subset(afs, City == input$city & State.Name == input$state)
+  #   }
+  #   DT::datatable(x[vars],options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
+  # })
 })
--- ui.R
+++ ui.R
@@ -20,25 +20,27 @@ body <- dashboardBody(
     ),
     column(width = 3,
            box(width = NULL, status = "warning",
-               p(
-                 class = "text-muted",
-                 paste("Please enter either the state and city, or enter the",
-                       "zip code below.  Note: if the zip code is entered, state",
-                       "and city are ignored."
-                 )
-               ),               
-               htmlOutput("stateUi"),
-               htmlOutput("cityUi"),
-               htmlOutput("zipUi"),
-               actionButton("refresh", "Render Map")
-           )
-    )
-  ),
-  fluidRow(
-    box(width = 9, status = "warning", 
-        tabPanel("Data Explorer", DT::dataTableOutput("table"))
-    )
+               textInput("location", label = h3("Location"), value = "Enter state, city and/or zip..."),
+               checkboxInput("currentLocation", label = "Use Current Location", value = TRUE)
+               ),
+           box(width = NULL, status = "warning",
+               h3("Fuel Types"),
+               checkboxInput("BD", label = "Biodiesel (B20 and above)", value = TRUE),
+               checkboxInput("CNG", label = "Compressed Natural Gas", value = TRUE),
+               checkboxInput("E85", label = "Ethanol (E85)", value = TRUE),
+               checkboxInput("ELEC", label = "Electric", value = TRUE),
+               checkboxInput("HY", label = "Hydrogen", value = TRUE),
+               checkboxInput("LNG", label = "Liquefied Natural Gas", value = TRUE),
+               checkboxInput("LPG", label = "Liquefied Petroleum Gas (Propane)", value = TRUE)               
+               ),
+           actionButton("refresh", "Render Map")
   )
+  # fluidRow(
+  #   box(width = 9, status = "warning", 
+  #       tabPanel("Data Explorer", DT::dataTableOutput("table"))
+  #   )
+  #)
+ )
 )
 
 dashboardPage(
